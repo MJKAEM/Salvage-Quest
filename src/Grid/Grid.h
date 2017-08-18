7ecfd5472aac87d4278e606e1b6b4a77bb4fd402
MJKAEM <Martinokuan@gmail.com>
2017-08-18 01:47:41 -0800

    Add header tags for grid data TXT. 1. Declare functions in Grid to reflect changes.

--- src/Grid/Grid.h
+++ src/Grid/Grid.h
@@ -1,6 +1,8 @@
 #ifndef GRID_GRID_H_
 #define GRID_GRID_H_
 
+#include <fstream>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -15,8 +17,12 @@ class Grid {
 private:
 	std::vector<std::string> areas;
 	unsigned long long width, height;
+	std::string dataFilePath;
 
 	Grid();
+	void ReadHeaders(std::istream&);
+	void ReadMetadata(std::istream&);
+	void ReadShortcuts(std::istream&);
 
 public:
 	Grid(std::string);
